c7c0cf79e5bb7efcc9fdbf95a9062ade13c3ce66
eliyaLevi <el0547135208@gmail.com>
2024-11-14 17:27:12 +0200

    added interface Missile

--- src/layout/users/Users.tsx
+++ src/layout/users/Users.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Login } from './login/Login'
 import { Register } from './register/Register'
 import { Org } from './org/org'
+import Store from './store/Store'
 
 export const Users = () => {
   return (
@@ -9,6 +10,7 @@ export const Users = () => {
        <Login/>
        <Register/>
        <Org/>
+       <Store/>
     </div>
   )
 }
